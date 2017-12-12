4d80aed957def34aad8102f668d65a5bf57a12c6
Dwayne Charrington <dwaynecharrington@gmail.com>
2017-12-12 23:21:43 +1000

    feat(app): edit and add mutators

--- src/app.js
+++ src/app.js
@@ -1,12 +1,24 @@
 import { Store } from 'aurelia-store';
 
-const addTodo = (state, todo) => {
+const addTodo = (state) => {
   const newState = Object.assign({}, state);
+  const newTodo = {
+    text: 'My todo'
+  };
 
-  newState.todos.push(todo);
+  newState.todos.push(newTodo);
+  newState.activeTodo = newTodo;
 
   return newState;
 }
+
+const editTodo = (state, text) => {
+  const newState = Object.assign({}, state);
+
+  newState.activeTodo.text = text;
+
+  return newState;
+};
 
 export class App {
   constructor() {
