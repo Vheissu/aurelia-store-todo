23eb3bdffd08ae7fb1af7e58eaf710980e85ff58
Dwayne Charrington <dwaynecharrington@gmail.com>
2017-12-13 21:07:18 +1000

    refact(app): don't create todo automatically on load

--- src/app.js
+++ src/app.js
@@ -1,21 +1,15 @@
 import { Store } from 'aurelia-store';
 
-import { addTodo } from './actions';
-
 export class App {
   static inject = [Store];
 
   constructor(store) {
     this.store = store;
-
-    // Add an initial todo
-    addTodo();
   }
 
   attached() {
     this.store.state.subscribe((state) => {
       this.state = state;
-      console.log(state);
     });
   }
 }
