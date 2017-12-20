a956ff98a2656eb124a03be1e15ff79fe3f57333
Zacharey Hollingshead <zach.hollingshead@gmail.com>
2017-12-20 15:28:55 -0600

    feat(ux): add aurelia-ux

--- src/app.js
+++ src/app.js
@@ -1,10 +1,18 @@
 import { Store } from 'aurelia-store';
+import { StyleEngine } from '@aurelia-ux/core';
+import { UxInputTheme } from '@aurelia-ux/input';
 
 export class App {
-  static inject = [Store];
+  static inject = [Store, StyleEngine];
 
-  constructor(store) {
+  constructor(store, styleEngine) {
     this.store = store;
+
+    const theme = new UxInputTheme();
+
+    theme.foreground = '#fff';
+
+    styleEngine.ensureDefaultTheme(theme);
   }
 
   attached() {
