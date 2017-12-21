48d6c3232a4fab0ace9f18fb4fadcafe1938985c
Dwayne Charrington <dwaynecharrington@gmail.com>
2017-12-21 10:49:24 +1000

    test(actions): start of testing actions

--- src/mutations.js
+++ src/mutations.js
@@ -3,6 +3,13 @@ import colours from './colours';
 
 import store from './store';
 
+/**
+ * Mutations are merely dispatched actions, the logic that 
+ * makes changes to your state management store is handled
+ * from within here.
+ * 
+ */
+
 function addTodo(state, text) {
   const newTodo = {
     id: guid(),
